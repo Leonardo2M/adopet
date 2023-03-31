9ad32a6d6371b5d034d117948e489e4125a87037
Leonardo2M <leonardomm.contato@gmail.com>
2023-03-31 00:50:31 -0300

    feat(TutorController): add new endpoint POST

--- api/src/main/java/br/com/adopet/api/controller/TutorController.java
+++ api/src/main/java/br/com/adopet/api/controller/TutorController.java
@@ -1,6 +1,12 @@
 package br.com.adopet.api.controller;
 
 import br.com.adopet.api.domain.service.TutorService;
+import br.com.adopet.api.dto.DadosCadastroTutor;
+import br.com.adopet.api.dto.TutorDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +20,10 @@ public class TutorController {
         this.service = service;
     }
 
-
+    @PostMapping
+    @Transactional
+    public ResponseEntity<TutorDTO> criarTutor(@RequestBody DadosCadastroTutor dados) {
+        return service.criarTutor(dados);
+    }
 
 }
