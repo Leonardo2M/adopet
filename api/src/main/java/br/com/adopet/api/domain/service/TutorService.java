6783e4c3aaff2e64b44f5c5ecf8a055c9591fea8
Leonardo2M <leonardomm.contato@gmail.com>
2023-03-31 00:38:39 -0300

    feat(TutorService): injecting ModelMapper into TutorService

--- api/src/main/java/br/com/adopet/api/domain/service/TutorService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/TutorService.java
@@ -1,15 +1,18 @@
 package br.com.adopet.api.domain.service;
 
 import br.com.adopet.api.domain.repository.TutorRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TutorService {
 
     private final TutorRepository repository;
+    private final ModelMapper modelMapper;
 
-    public TutorService(TutorRepository repository) {
+    public TutorService(TutorRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
+        this.modelMapper = modelMapper;
     }
 
 }
