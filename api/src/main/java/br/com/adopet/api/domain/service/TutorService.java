8183f80e967412ee9442396f7ee579e72bc5a841
Leonardo2M <leonardomm.contato@gmail.com>
2023-03-31 00:41:27 -0300

    feat(TutorService): add method createTutor

--- api/src/main/java/br/com/adopet/api/domain/service/TutorService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/TutorService.java
@@ -1,8 +1,14 @@
 package br.com.adopet.api.domain.service;
 
+import br.com.adopet.api.domain.model.Tutor;
 import br.com.adopet.api.domain.repository.TutorRepository;
+import br.com.adopet.api.dto.DadosCadastroTutor;
+import br.com.adopet.api.dto.TutorDTO;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Service
 public class TutorService {
@@ -13,6 +19,12 @@ public class TutorService {
     public TutorService(TutorRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
         this.modelMapper = modelMapper;
+    }
+
+    public ResponseEntity<TutorDTO> criarTutor(DadosCadastroTutor dados) {
+        var tutor = modelMapper.map(dados, Tutor.class);
+        repository.save(tutor);
+        return ResponseEntity.ok().body(modelMapper.map(tutor, TutorDTO.class));
     }
 
 }
