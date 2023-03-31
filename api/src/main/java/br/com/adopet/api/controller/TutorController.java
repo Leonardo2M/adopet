7d81851be1d5a7b0547c05a77d5d6fb48ab1df9d
Leonardo2M <leonardomm.contato@gmail.com>
2023-03-31 00:14:32 -0300

    feat(TutorController): add new controller for Tutor

---
+++ api/src/main/java/br/com/adopet/api/controller/TutorController.java
@@ -0,0 +1,17 @@
+package br.com.adopet.api.controller;
+
+import br.com.adopet.api.domain.service.TutorService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tutor")
+public class TutorController {
+
+    private final TutorService service;
+
+    public TutorController(TutorService service) {
+        this.service = service;
+    }
+
+}
