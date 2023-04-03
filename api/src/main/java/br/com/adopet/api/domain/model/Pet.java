32030bf34b68f8dddedeced6c7fb39b2d0eba82d
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-03 20:16:06 -0300

    feat(Pet): add new entity Pet

---
+++ api/src/main/java/br/com/adopet/api/domain/model/Pet.java
@@ -0,0 +1,27 @@
+package br.com.adopet.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descrição;
+    private Boolean adotado;
+    private Integer idade;
+    private String endereço;
+    private String imagem;
+
+}
