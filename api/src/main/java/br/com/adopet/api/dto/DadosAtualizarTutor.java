d360c29c648291126f58a3222fa176b8e2364f8d
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-03 20:04:39 -0300

    refactor: adding validations in the dto

--- api/src/main/java/br/com/adopet/api/dto/DadosAtualizarTutor.java
+++ api/src/main/java/br/com/adopet/api/dto/DadosAtualizarTutor.java
@@ -1,5 +1,7 @@
 package br.com.adopet.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DadosAtualizarTutor {
 
+    @NotBlank
     private String nome;
+    @NotBlank
+    @Pattern(regexp = "\\(\\d{2}\\)\\d{5}-\\d{4}")
     private String telefone;
+    @NotBlank
     private String cidade;
+    @NotBlank
     private String sobre;
 
 }
