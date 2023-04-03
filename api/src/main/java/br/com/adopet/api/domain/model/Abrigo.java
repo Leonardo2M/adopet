13f36e86b58599338fdf8e5b817a0cd4be1811e0
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-03 20:23:19 -0300

    feat(Pet): add new entity Abrigo

---
+++ api/src/main/java/br/com/adopet/api/domain/model/Abrigo.java
@@ -0,0 +1,25 @@
+package br.com.adopet.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "tutores")
+public class Abrigo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+}
