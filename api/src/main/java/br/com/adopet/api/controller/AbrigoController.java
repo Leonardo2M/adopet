1c8034680d76c63e7db06747eccdb9ac2bf6407f
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-04 11:41:40 -0300

    feat(AbrigoController): add new endpoint GET abrigos/id

--- api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
+++ api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
@@ -4,13 +4,10 @@ import br.com.adopet.api.domain.service.AbrigoService;
 import br.com.adopet.api.dto.abrigo.AbrigoDTO;
 import br.com.adopet.api.dto.abrigo.DadosCadatroAbrigo;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/{abrigos}")
+@RequestMapping("/abrigos")
 public class AbrigoController {
 
     private final AbrigoService service;
@@ -22,5 +19,9 @@ public class AbrigoController {
     @PostMapping
     public ResponseEntity<AbrigoDTO> criar(@RequestBody DadosCadatroAbrigo dados) {
         return service.criar(dados);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<AbrigoDTO> buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
     }
 }
