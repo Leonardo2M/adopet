8388c389f5ac4197201122f5110a337ab5ef77c6
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-04 11:19:29 -0300

    feat(AbrigoService): add new method to create abrigo

--- api/src/main/java/br/com/adopet/api/domain/service/AbrigosService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/AbrigosService.java
@@ -1,15 +1,30 @@
 package br.com.adopet.api.domain.service;
 
+import br.com.adopet.api.domain.model.Abrigo;
 import br.com.adopet.api.domain.repository.abrigo.AbrigoRepository;
+import br.com.adopet.api.dto.abrigo.AbrigoDTO;
+import br.com.adopet.api.dto.abrigo.DadosCadatroAbrigo;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AbrigosService {
 
     private final AbrigoRepository repository;
+    private final ModelMapper modelMapper;
 
-    public AbrigosService(AbrigoRepository repository) {
+    public AbrigosService(AbrigoRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
+        this.modelMapper = modelMapper;
     }
+
+    public ResponseEntity<AbrigoDTO> criar(DadosCadatroAbrigo dados) {
+        var abrigo = modelMapper.map(dados, Abrigo.class);
+        repository.save(abrigo);
+
+        return ResponseEntity.ok().body(modelMapper.map(abrigo, AbrigoDTO.class));
+    }
+
 
 }
