5ffd54e4d32eb5480c7c693874080775e00aabcc
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-04 12:03:34 -0300

    feat(AbrigoController): new endpoint GET /abrigos

--- api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
+++ api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
@@ -3,8 +3,13 @@ package br.com.adopet.api.controller;
 import br.com.adopet.api.domain.service.AbrigoService;
 import br.com.adopet.api.dto.abrigo.AbrigoDTO;
 import br.com.adopet.api.dto.abrigo.DadosCadatroAbrigo;
+import br.com.adopet.api.dto.abrigo.DadosListagemAbrigo;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/abrigos")
@@ -17,11 +22,18 @@ public class AbrigoController {
     }
 
     @PostMapping
-    public ResponseEntity<AbrigoDTO> criar(@RequestBody DadosCadatroAbrigo dados) {
+    @Transactional
+    public ResponseEntity<AbrigoDTO> criar(@RequestBody @Valid DadosCadatroAbrigo dados) {
         return service.criar(dados);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<AbrigoDTO> buscarPorId(@PathVariable Long id) {
         return service.buscarPorId(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DadosListagemAbrigo>> buscarTodos() {
+        return service.buscarTodos();
     }
 }
