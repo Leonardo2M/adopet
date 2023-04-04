be33a586058463f4d749619f7b4d00d2505d943f
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-04 12:19:47 -0300

    feat(AbrigoController): add new endpoint PUT

--- api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
+++ api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
@@ -2,10 +2,13 @@ package br.com.adopet.api.controller;
 
 import br.com.adopet.api.domain.service.AbrigoService;
 import br.com.adopet.api.dto.abrigo.AbrigoDTO;
+import br.com.adopet.api.dto.abrigo.DadosAtualizarAbrigo;
 import br.com.adopet.api.dto.abrigo.DadosCadatroAbrigo;
 import br.com.adopet.api.dto.abrigo.DadosListagemAbrigo;
+import br.com.adopet.api.dto.tutor.DadosAtualizarTutor;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,4 +39,11 @@ public class AbrigoController {
     public ResponseEntity<List<DadosListagemAbrigo>> buscarTodos() {
         return service.buscarTodos();
     }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<AbrigoDTO> ataulizar(@PathVariable Long id, @RequestBody @Valid DadosAtualizarAbrigo dados) {
+        return service.atualizar(dados, id);
+    }
+
 }
