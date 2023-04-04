2e9620ceaa41ba17a91c0b8de6197441afe72a1a
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-04 11:28:27 -0300

    feat(AbrigoController): add new endpoint POST

--- api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
+++ api/src/main/java/br/com/adopet/api/controller/AbrigoController.java
@@ -1,6 +1,11 @@
 package br.com.adopet.api.controller;
 
 import br.com.adopet.api.domain.service.AbrigoService;
+import br.com.adopet.api.dto.abrigo.AbrigoDTO;
+import br.com.adopet.api.dto.abrigo.DadosCadatroAbrigo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,8 @@ public class AbrigoController {
         this.service = service;
     }
 
-
+    @PostMapping
+    public ResponseEntity<AbrigoDTO> criar(@RequestBody DadosCadatroAbrigo dados) {
+        return service.criar(dados);
+    }
 }
