cd0d49f54976bba378c6e3010936d06d27bb08e4
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-05 11:18:18 -0300

    feat(PetService): added new method to create pet

--- api/src/main/java/br/com/adopet/api/domain/service/PetService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/PetService.java
@@ -1,14 +1,28 @@
 package br.com.adopet.api.domain.service;
 
+import br.com.adopet.api.domain.model.Pet;
 import br.com.adopet.api.domain.repository.PetRepository;
+import br.com.adopet.api.dto.pet.DadosCadastroPet;
+import br.com.adopet.api.dto.pet.PetDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetService {
 
     private final PetRepository repository;
+    private final ModelMapper modelMapper;
 
-    public PetService(PetRepository repository) {
+    public PetService(PetRepository repository, ModelMapper modelMapper) {
         this.repository = repository;
+        this.modelMapper = modelMapper;
+    }
+
+    public ResponseEntity<PetDTO> criar(DadosCadastroPet dados) {
+        var pet = modelMapper.map(dados, Pet.class);
+        repository.save(pet);
+
+        return ResponseEntity.ok().body(modelMapper.map(pet, PetDTO.class));
     }
 }
