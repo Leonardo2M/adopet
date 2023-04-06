88d79048cf329b0ced72fa1abf739380b2b652be
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-05 22:02:34 -0300

    feat(PetController): add new endpoint PUT

--- api/src/main/java/br/com/adopet/api/controller/PetController.java
+++ api/src/main/java/br/com/adopet/api/controller/PetController.java
@@ -1,6 +1,7 @@
 package br.com.adopet.api.controller;
 
 import br.com.adopet.api.domain.service.PetService;
+import br.com.adopet.api.dto.pet.DadosAtualizarPet;
 import br.com.adopet.api.dto.pet.DadosCadastroPet;
 import br.com.adopet.api.dto.pet.DadosListagemPet;
 import br.com.adopet.api.dto.pet.PetDTO;
@@ -36,6 +37,12 @@ public class PetController {
     @GetMapping
     public ResponseEntity<List<DadosListagemPet>> buscarTodos() {
         return service.buscarTodos();
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<PetDTO> atualizar(@RequestBody DadosAtualizarPet dados, @PathVariable Long id) {
+        return service.atualizar(dados, id);
     }
 
 }
