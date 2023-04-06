a93eba5a343abe98a90971483e5744ac706cdf29
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-06 20:26:46 -0300

    feat(Adocao): add new entity Adocao

---
+++ api/src/main/java/br/com/adopet/api/domain/model/Adocao.java
@@ -0,0 +1,27 @@
+package br.com.adopet.api.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name= "adocao")
+public class Adocao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Pet pet;
+    @OneToOne
+    private Tutor tutor;
+    private LocalDateTime data = LocalDateTime.now();
+}
