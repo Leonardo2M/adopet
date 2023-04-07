d583c05571fe10841fd001b629413e0ddcbed81c
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-06 21:12:19 -0300

    feat(AdocaoController): add new endpoint POST

--- api/src/main/java/br/com/adopet/api/controller/AdocaoController.java
+++ api/src/main/java/br/com/adopet/api/controller/AdocaoController.java
@@ -1,6 +1,12 @@
 package br.com.adopet.api.controller;
 
 import br.com.adopet.api.domain.service.AdocaoService;
+import br.com.adopet.api.dto.doacao.AdocaoDTO;
+import br.com.adopet.api.dto.doacao.DadosRealizarAdocao;
+import jakarta.transaction.Transactional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,10 @@ public class AdocaoController {
         this.service = service;
     }
 
+    @PostMapping
+    @Transactional
+    public ResponseEntity<AdocaoDTO> adotar(@RequestBody DadosRealizarAdocao dados) {
+        return service.adotar(dados);
+    }
 
 }
