c4199b0f7190dbb8b0b90626f26cab9fb3fffe39
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-17 21:24:52 -0300

    feat(SecurityConfig): implementing authenticationManager bean

--- api/src/main/java/br/com/adopet/api/config/SecurityConfig.java
+++ api/src/main/java/br/com/adopet/api/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package br.com.adopet.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +18,11 @@ public class SecurityConfig {
                 .and().authorizeHttpRequests()
                 .requestMatchers("/**").permitAll()
                 .and().build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
