03d1e7ef5b2bd1aca4883f1e8ada61591cc46410
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-19 20:23:25 -0300

    feat(TokenService): getting secret through application.properties

--- api/src/main/java/br/com/adopet/api/domain/service/security/TokenService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.adopet.api.domain.model.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -13,10 +14,12 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    public String gerarToken(Usuario usuario) {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
+    public String gerarToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256("VARIAVEL AMBIENTE");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Adopet")
                     .withSubject(usuario.getUsername())
