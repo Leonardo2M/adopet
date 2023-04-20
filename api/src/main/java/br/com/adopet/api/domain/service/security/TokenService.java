bd068e125a0586e681a216006cf03d3ac8bcfea0
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-19 21:13:37 -0300

    feat(SecurityFilter): new filter for access control

--- api/src/main/java/br/com/adopet/api/domain/service/security/TokenService.java
+++ api/src/main/java/br/com/adopet/api/domain/service/security/TokenService.java
@@ -2,8 +2,11 @@ package br.com.adopet.api.domain.service.security;
 
 import br.com.adopet.api.domain.model.usuario.Usuario;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +33,21 @@ public class TokenService {
 
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        DecodedJWT decodedJWT;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    // specify an specific claim validations
+                    .withIssuer("API Adopet")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
