b6d26fead3494a14625eac28f35df5750d26e649
Leonardo2M <leonardomm.contato@gmail.com>
2023-04-27 21:18:57 -0300

    feat(ValidacoesCancelamento): created interface to validate cancellation

---
+++ api/src/main/java/br/com/adopet/api/domain/model/adocao/validacoes/cancelar/ValidacoesCancelamento.java
@@ -0,0 +1,9 @@
+package br.com.adopet.api.domain.model.adocao.validacoes.cancelar;
+
+import br.com.adopet.api.dto.doacao.DadosRealizarAdocao;
+
+public interface ValidacoesCancelamento {
+
+    void validar();
+
+}
